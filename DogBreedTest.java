004b282c1db16c49da95f7a06caff106c26dc559
Daniel Waggoner <D.Waggoner@outlook.com>
2017-03-18 18:05:00 -0400

    changed order of choosing name and breed

--- DogBreedTest.java
+++ DogBreedTest.java
@@ -12,10 +12,11 @@ class DogBreedTest {
   public static void main(String args[]){
     Scanner keyboardInput = new Scanner(System.in);
     Dog doggo = new Dog();
-    System.out.print("Name your new pupper: ");
-    doggo.name = keyboardInput.nextLine();
     System.out.print("What kind of pupper do you want - a corgi, a shibu, or a husky? ");  
     doggo.breed = Dog.DogBreed.valueOf(keyboardInput.nextLine());
+    System.out.print("Name your new pupper: ");
+    doggo.name = keyboardInput.nextLine();
+
     System.out.println("Breed of " + doggo.name + " is: " + doggo.breed);
   }
 }
